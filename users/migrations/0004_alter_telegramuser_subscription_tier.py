1bc8f5feee866d71b609557f018cd824314ed8aa
o2o <Mavinsplay@yandex.ru>
2026-07-19 21:59:58 +0300

    fix: use raw SQL to swap subscription_tier column type

--- users/migrations/0004_alter_telegramuser_subscription_tier.py
+++ users/migrations/0004_alter_telegramuser_subscription_tier.py
@@ -2,11 +2,6 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-def clear_tier_text(apps, schema_editor):
-    TelegramUser = apps.get_model("users", "TelegramUser")
-    TelegramUser.objects.update(subscription_tier=None)
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -15,16 +10,31 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(clear_tier_text, migrations.RunPython.noop),
-        migrations.AlterField(
-            model_name="telegramuser",
-            name="subscription_tier",
-            field=models.ForeignKey(
-                blank=True,
-                null=True,
-                on_delete=django.db.models.deletion.SET_NULL,
-                to="payments.pricingplan",
-                verbose_name="Тариф",
-            ),
+        migrations.SeparateDatabaseAndState(
+            state_operations=[
+                migrations.AlterField(
+                    model_name="telegramuser",
+                    name="subscription_tier",
+                    field=models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to="payments.pricingplan",
+                        verbose_name="Тариф",
+                    ),
+                ),
+            ],
+            database_operations=[
+                migrations.RunSQL(
+                    sql=[
+                        'ALTER TABLE users_telegramuser DROP COLUMN subscription_tier;',
+                        'ALTER TABLE users_telegramuser ADD COLUMN subscription_tier_id integer NULL REFERENCES payments_pricingplan(id) ON DELETE SET NULL;',
+                    ],
+                    reverse_sql=[
+                        'ALTER TABLE users_telegramuser DROP COLUMN subscription_tier_id;',
+                        "ALTER TABLE users_telegramuser ADD COLUMN subscription_tier varchar(50) NOT NULL DEFAULT '';",
+                    ],
+                ),
+            ],
         ),
     ]
